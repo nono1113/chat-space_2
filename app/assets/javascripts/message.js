78442f721d938c7f931f2854e32a77585ec54905
Miho Hamanaka <2081050.nono@gmail.com>
2019-05-17 21:32:35 +0900

    Second commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,20 @@
 $(document).on("turbolinks:load", function () {
   function buildHTML(message) {
-    image = (message.image) ? `<image class="lower-message__image" src= ${message.image}>` : " ";
+    var image = ``
+    if (message.image) {
+      var image = `<image class="lower-message__image" src= "${message.image}">`
+    }
     var html = `<div class="message">
                   <div class="upper-message">
                     <div class= "upper-message__user-name">
                       ${message.user_name}
                     </div>
                     <div class= "upper-message__date">
-                      ${message.date}
+                      ${message.created_at}
                     </div>
                   </div>
                   <div class = "lower-message">
-                    <p class = "lower-message__content">
+                    <p class = "lower-message__content" data-id=${message.id}>
                       ${message.content}
                     </p>
                     <div>
@@ -24,6 +27,7 @@ $(document).on("turbolinks:load", function () {
 
   $('.msg_form').on('submit', function (e) {
     e.preventDefault();
+
     var formData = new FormData(this);
     var url = window.location.href;
     $.ajax({
@@ -48,5 +52,34 @@ $(document).on("turbolinks:load", function () {
       .fail(function () {
         alert('error');
       })
-  })
-})
+    var reloadMessages = function () {
+      lastMessage = $(".lower-message__content").filter(":last");
+      last_message_id = lastMessage.attr('data-id')
+      if (lastMessage.attr('data-id')) {
+        $.ajax({
+            url: 'api/messages',
+            type: 'get',
+            dataType: 'json',
+            data: {
+              id: last_message_id
+            }
+          })
+          .done(function (messages) {
+            if (messages.length !== 0) {
+              messages.forEach(function (message) {
+                var html = buildHTML(message);
+                $('.messages').append(html)
+              });
+              $('.messages').animate({
+                scrollTop: $('.messages')[0].scrollHeight
+              }, 'fast');
+            }
+          })
+          .fail(function () {
+            console.log('error');
+          });
+      }
+    };
+    setInterval(reloadMessages, 5000);
+  });
+});
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,6 +1,6 @@
 json.user_name @message.user.name
-json.date      @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.created_at      @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content   @message.content
 json.image     @message.image.url
-
+json.id        @message.id
 
