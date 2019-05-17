cc43808152756ec1c87f2ebe72b6f4987e97566b
Miho Hamanaka <2081050.nono@gmail.com>
2019-05-17 11:59:58 +0900

    Mod2_Second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id:current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")if params[:keyword].present?
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id:current_user.id)if params[:keyword].present?
     respond_to do |format|
       format.json
     end
