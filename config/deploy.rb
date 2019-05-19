6bae9d2239122c09b502905d09e6f8b16f03634b
Miho Hamanaka <2081050.nono@gmail.com>
2019-05-19 14:24:51 +0900

    Capistrano_setting4

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space_2"
-set :repo_url, "git@example.com:nono1113/chat-space_2.git"
+set :repo_url, "git@github.com:nono1113/chat-space_2.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
